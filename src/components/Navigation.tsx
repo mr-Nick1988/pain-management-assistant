96f9956931094b3fa9ce4dab2da306f06d058335
Nick <andreeffnick2@gmail.com>
2025-09-15 20:44:23 +0300

    navigation for doctor add

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { UserRole } from '../types/personRegister.ts';
 
 const Navigation: React.FC = () => {
     const navigate = useNavigate();
     const isAuthenticated = localStorage.getItem('token') !== null;
+    const userRole = localStorage.getItem('userRole');
 
     const handleLogout = () => {
         localStorage.removeItem('token');
+        localStorage.removeItem('userRole');
         navigate('/login');
     };
 
@@ -17,9 +20,16 @@ const Navigation: React.FC = () => {
     return (
         <nav className="navigation">
             <ul>
-                <li>
-                    <Link to="/admin">Admin Panel</Link>
-                </li>
+                {userRole === UserRole.ADMIN && (
+                    <li>
+                        <Link to="/admin">Admin Panel</Link>
+                    </li>
+                )}
+                {userRole === UserRole.DOCTOR && (
+                    <li>
+                        <Link to="/doctor">Doctor Dashboard</Link>
+                    </li>
+                )}
                 <li>
                     <Link to="/change-credentials">Change Credentials</Link>
                 </li>
