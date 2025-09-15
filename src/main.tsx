a105d27023f80445e487c42424d08814cf1c2264
Nick <andreeffnick2@gmail.com>
2025-09-15 21:01:47 +0300

    store fixing

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {store} from "../store.ts";
+import {store} from "./store/store.ts";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
